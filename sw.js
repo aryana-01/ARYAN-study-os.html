a762de76e2a81ecfe3e784dea66eb261a9eddcd4
aryana-01 <raryann11@gmail.com>
2026-03-13 23:47:24 +0530

    Implement app install prompt handling
    Added functionality for handling app installation prompts.

--- sw.js
+++ sw.js
@@ -20,3 +20,28 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  installBtn.style.display = "block";
+});
+
+installBtn.addEventListener("click", async () => {
+
+  if (!deferredPrompt) return;
+
+  deferredPrompt.prompt();
+
+  const { outcome } = await deferredPrompt.userChoice;
+
+  if (outcome === "accepted") {
+    console.log("App installed");
+  }
+
+  deferredPrompt = null;
+  installBtn.style.display = "none";
+});
