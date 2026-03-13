20ea5adcac2305ecc11c8f2c42ec341fbd442613
aryana-01 <raryann11@gmail.com>
2026-03-13 23:52:06 +0530

    Refactor service worker caching and installation logic

--- sw.js
+++ sw.js
@@ -13,35 +13,6 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-let deferredPrompt;
-const installBtn = document.getElementById("installBtn");
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-
-  installBtn.style.display = "block";
-});
-
-installBtn.addEventListener("click", async () => {
-
-  if (!deferredPrompt) return;
-
-  deferredPrompt.prompt();
-
-  const { outcome } = await deferredPrompt.userChoice;
-
-  if (outcome === "accepted") {
-    console.log("App installed");
-  }
-
-  deferredPrompt = null;
+Prompt = null;
   installBtn.style.display = "none";
 });
